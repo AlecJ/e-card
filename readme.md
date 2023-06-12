dcd8ad0c8ea6d885cd1a7f3fb4961e4247ddbd64
Alec Jordan <alecbjordan@gmail.com>
2023-06-11 22:22:50 -0400

    Development Setup
    Development Setup.
    
    Run front end (vue) and backend (express) in separate containers with hot reload for each.

---
+++ readme.md
@@ -0,0 +1,14 @@
+E-Card
+
+## Setup
+
+Run the dev environment:
+`docker-compose up`
+
+Navigate to:
+http://0.0.0.0:3000
+
+### Production
+
+Run the prod environment:
+TODO
--- src/server/app.js
+++ src/server/app.js
@@ -1,36 +1,41 @@
 const Express = require("express")();
 const Http = require("http").Server(Express);
-const Socketio = require("socket.io")(Http);
+const Socketio = require("socket.io")(Http, { path: '/api' });
+
+const HOST = '0.0.0.0'
+const PORT = 8080
 
 var position = {
     x: 200,
     y: 200
 };
 
-Http.listen(3000, () => {
-    console.log("Listening at :3000...");
+Http.listen(PORT, HOST, () => {
+    console.log(`Listening at ${HOST}:${PORT}`);
 });
 
 Socketio.on("connection", socket => {
+    console.log('visitor!');
     // send current position to joining players
     socket.emit("position", position);
+    console.log(position);
 
     socket.on("move", data => {
         switch(data) {
             case "left":
-                position.x -= 5;
+                position.x -= 15;
                 Socketio.emit("position", position);
                 break;
             case "right":
-                position.x += 5;
+                position.x += 15;
                 Socketio.emit("position", position);
                 break;
             case "up":
-                position.y -= 5;
+                position.y -= 15;
                 Socketio.emit("position", position);
                 break;
             case "down":
-                position.y += 5;
+                position.y += 15;
                 Socketio.emit("position", position);
                 break;
         }
